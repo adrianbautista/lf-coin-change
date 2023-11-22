f78af1442941c57ae5aa20a3ddd3d5fb68efa2cc
Adrian Bautista <adrianbautista@users.noreply.github.com>
2023-11-22 12:03:08 -0500

    Fix examples

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
  *  LEARNING FUZE MOCK TECHNICAL INTERVIEW:
  *  COIN CHANGE PROBLEM
  *
- *  Given an amount (in cents), write a function that returns    
+ *  Given an amount (in cents), write a function that returns
  *  the minimum number of coins needed and specifies which coins.
  *
- *  To begin, we'll default to US coin denominations                       
- *  (pennies, nickels, dimes, and quarters).                     
+ *  To begin, we'll default to US coin denominations
+ *  (pennies, nickels, dimes, and quarters).
  *
  *  Example:
- *  For 27 cents: 5 coins are needed ([25, 1, 1])
+ *  For 27 cents: 3 coins are needed ([25, 1, 1])
  *
  *  NOTES & TIPS:
  *  - Please disable Co-Pilot or any AI autocomplete before starting.
@@ -25,15 +25,11 @@
 
 const US_COINS = [25, 10, 5, 1];
 
-let amountCents;
 let numberOfCoins;
 let coinsNeeded;
 
 function coinChange(amountCents, coins = US_COINS) {
-
 }
-
-
 
 /****************
  *
@@ -41,9 +37,8 @@ function coinChange(amountCents, coins = US_COINS) {
  *  TEST CASE 1
  *
 ****************/
-// amountCents = 33
-// coinChange(33);
-// console.log(`For ${amountCents} cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
+coinChange(33);
+console.log(`For 33 cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
 
 /****************
  *
@@ -51,9 +46,8 @@ function coinChange(amountCents, coins = US_COINS) {
  *  TEST CASE 2
  *
 ****************/
-// amountCents = 11
 // coinChange(11);
-// console.log(`For ${amountCents} cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
+// console.log(`For 11 cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
 
 /****************
  *
@@ -61,6 +55,5 @@ function coinChange(amountCents, coins = US_COINS) {
  *  TEST CASE 3
  *
 ****************/
-// amountCents = 11
 // coinChange(11, [9, 6, 5, 1]);
-// console.log(`For ${amountCents} cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
+// console.log(`For 11 cents: ${numberOfCoins} coins are needed (${coinsNeeded})`);
